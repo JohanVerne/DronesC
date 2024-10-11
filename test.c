2c16f19962e26281dd0d51166b9cafde634ab8f1
eq159 <quereemilien@gmail.com>
2024-10-11 10:33:57 +0200

    fenêtre

--- test.c
+++ test.c
@@ -1,163 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <SDL2/SDL.h>  
-#include <SDL2/SDL_image.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>  // Bibliothèque pour charger des images
 
 // Structure représentant un drone
-typedef struct
-{
+typedef struct {
     int id;           // Identifiant unique du drone
-    float x, y, z;    // Position actuelle du drone (XYZ)
-    float x_prev, y_prev, z_prev; // Position précédente du drone
+    float x, y, z;    // Position du drone (XYZ)
     float vitesse;    // Vitesse de déplacement du drone
     float portee_com; // Portée de communication du drone
     int actif;        // Indique si le drone est actif ou détruit
+    float prev_x, prev_y;
+    float taille;  // Position précédente du drone pour laisser le tracé
 } Drone;
 
-// Structure représentant la zone à surveiller
-typedef struct
-{
-    float xMin, yMin, xMax, yMax; // Coordonnées de la zone
-} Zone;
-
 // Initialisation d'un drone
-void init_drone(Drone *drone, int id, float x, float y, float z, float vitesse, float portee_com)
-{
+void init_drone(Drone *drone, int id, float x, float y, float z, float vitesse, float portee_com, float taille) {
     drone->id = id;
     drone->x = x;
     drone->y = y;
     drone->z = z;
-    drone->x_prev = x; // Initialement, la position précédente est la même que la position actuelle
-    drone->y_prev = y;
-    drone->z_prev = z;
     drone->vitesse = vitesse;
+    drone->taille = taille;
     drone->portee_com = portee_com;
     drone->actif = 1; // Drone actif par défaut
+    drone->prev_x = x; // Initialiser la position précédente
+    drone->prev_y = y; // Initialiser la position précédente
     printf("Drone %d initialisé à la position (%.2f, %.2f, %.2f) avec vitesse %.2f et portée %.2f\n", id, x, y, z, vitesse, portee_com);
 }
 
-// Définition de la zone à surveiller
-void definir_zone(Zone *zone, float xMin, float yMin, float xMax, float yMax)
-{
-    zone->xMin = xMin;
-    zone->yMin = yMin;
-    zone->xMax = xMax;
-    zone->yMax = yMax;
-    printf("Zone définie : de (%.2f, %.2f) à (%.2f, %.2f)\n", xMin, yMin, xMax, yMax);
-}
-
 // Déplacement des drones (mise à jour des positions)
-void deplacer_drones(Drone *drones, int nb_drones)
-{
-    for (int i = 0; i < nb_drones; i++)
-    {
-        if (drones[i].actif)
-        {
-            // Stocker la position précédente
-            drones[i].x_prev = drones[i].x;
-            drones[i].y_prev = drones[i].y;
-            drones[i].z_prev = drones[i].z;
+void deplacer_drones(Drone *drones, int nb_drones) {
+    for (int i = 0; i < nb_drones; i++) {
+        drones[i].prev_x = drones[i].x; // Enregistrer la position précédente
+        drones[i].prev_y = drones[i].y;
 
-            // Simuler un déplacement simple pour chaque drone
-            drones[i].x += drones[i].vitesse; // Déplacement à droite en fonction de la vitesse
-            drones[i].y += drones[i].vitesse; // Déplacement vers le bas en fonction de la vitesse
+        // Simuler un déplacement simple pour chaque drone
+        drones[i].x += drones[i].vitesse; // Déplacement à droite en fonction de la vitesse
+        drones[i].y += drones[i].vitesse; // Déplacement vers le bas en fonction de la vitesse
 
-            // Vérifier que les drones ne sortent pas de la fenêtre (800x600)
-            if (drones[i].x > 790) drones[i].x = 10; // Réinitialiser la position X si trop loin
-            if (drones[i].y > 590) drones[i].y = 10; // Réinitialiser la position Y si trop loin
-
-            printf("Drone %d déplacé à la position (%.2f, %.2f, %.2f)\n", drones[i].id, drones[i].x, drones[i].y, drones[i].z);
-        }
-        else
-        {
-            printf("Drone %d est inactif\n", drones[i].id);
-        }
+        // Vérifier que les drones ne sortent pas de la fenêtre (800x600)
+        if (drones[i].x > 790) drones[i].x = 10; // Réinitialiser la position X si trop loin
+        if (drones[i].y > 590) drones[i].y = 10; // Réinitialiser la position Y si trop loin
     }
 }
 
-// Vérifie si le drone1 est à portée de communication du drone2
-int est_voisin(Drone *drone1, Drone *drone2)
-{
-    float distance = sqrt(pow(drone1->x - drone2->x, 2) + pow(drone1->y - drone2->y, 2) + pow(drone1->z - drone2->z, 2));
-    return distance <= drone1->portee_com;
-}
+// Dessiner le drone et effacer le masque noir à sa nouvelle position
+void reveal_map(SDL_Renderer *renderer, SDL_Texture *map_texture, Drone *drone) {
+    // Afficher la carte à l'endroit où le drone passe (effacer le masque noir)
+    SDL_Rect src_rect = { (int)drone->x, (int)drone->y, ((int)drone->taille)*2, ((int)drone->taille)*2 }; // Zone à dévoiler
+    SDL_RenderCopy(renderer, map_texture, &src_rect, &src_rect);
 
-// Capture d'une image par un drone
-void capturer_image(Drone *drone)
-{
-    if (drone->actif)
-    {
-        printf("Drone %d capture une image à la position (%.2f, %.2f, %.2f)\n", drone->id, drone->x, drone->y, drone->z);
-        // TODO : Code pour générer une image (en fonction des specs)
-    }
-    else
-    {
-        printf("Drone %d est inactif et ne peut pas capturer d'image.\n", drone->id);
-    }
-}
-
-// Gestion de la destruction d'un drone (désactivation)
-void detruire_drone(Drone *drone)
-{
-    drone->actif = 0;
-    printf("Drone %d a été détruit.\n", drone->id);
-}
-
-// Fonction pour dessiner un drone
-void draw_drone(SDL_Renderer *renderer, Drone *drone)
-{
-    // Dessiner le drone
+    // Dessiner le drone à sa nouvelle position
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); // Couleur rouge pour les drones
-    SDL_Rect rect = { (int)drone->x, (int)drone->y, 15, 15 }; // Taille du drone
+    SDL_Rect rect = { (int)drone->x, (int)drone->y, (int)drone->taille, (int)drone->taille }; // Taille du drone
     SDL_RenderFillRect(renderer, &rect);
 }
 
-// Fonction pour dessiner le tracé du drone
-void draw_trail(SDL_Renderer *renderer, Drone *drone)
-{
-    // Dessiner une ligne entre l'ancienne position et la nouvelle position
-    SDL_SetRenderDrawColor(renderer, 139, 69, 19, 255); // Couleur marron pour le tracé
-    SDL_RenderDrawLine(renderer, (int)drone->x_prev + 5, (int)drone->y_prev + 5, (int)drone->x + 5, (int)drone->y + 5);
-}
+int main() {
 
-// Simulation de la gestion des drones
-int main()
-{
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-    {
-        printf("Erreur lors de l'initialisation de SDL: %s\n", SDL_GetError());
-        return 1;
-    }
+    // Initialiser SDL et SDL_image
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);  // Pour charger une image PNG
 
     SDL_Window *window = SDL_CreateWindow("Surveillance par Drones",
                                           SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                                           800, 600, SDL_WINDOW_SHOWN);
 
-    if (!window)
-    {
-        printf("Erreur lors de la création de la fenêtre: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    if (!renderer)
-    {
-        printf("Erreur lors de la création du renderer: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
+    // Charger l'image de la carte
     SDL_Surface *image_surface = IMG_Load("carte.png");  // Charger l'image depuis un fichier
     if (!image_surface) {
         printf("Erreur de chargement de l'image: %s\n", IMG_GetError());
         return 1;
     }
 
-    SDL_Texture *background_texture = SDL_CreateTextureFromSurface(renderer, image_surface);
+    // Convertir l'image en texture pour l'afficher
+    SDL_Texture *map_texture = SDL_CreateTextureFromSurface(renderer, image_surface);
     SDL_FreeSurface(image_surface);  // Libérer la surface maintenant que nous avons la texture
 
     // Création de trois drones pour la démonstration
@@ -165,25 +86,17 @@ int main()
     Drone drones[nb_drones];
 
     // Initialisation des drones avec des paramètres arbitraires
-    init_drone(&drones[0], 1, 750.0, 20.0, 5.0, 1.5, 30.0);
-    init_drone(&drones[1], 2, 30.0, 20.0, 5.0, 1.2, 25.0);
-    init_drone(&drones[2], 3, 30.0, 550.0, 5.0, 1.8, 35.0);
+    init_drone(&drones[0], 1, 750.0, 20.0, 5.0, 1.5, 30.0, 10);
+    init_drone(&drones[1], 2, 30.0, 20.0, 5.0, 1.2, 25.0, 20);
+    init_drone(&drones[2], 3, 30.0, 550.0, 5.0, 1.8, 35.0, 15);
 
     int running = 1;
     SDL_Event event;
 
-    // Effacer l'écran une fois au début pour le fond
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Noir pour l'arrière-plan
-    SDL_RenderClear(renderer);
-
-    // Boucle principale
-    while (running)
-    {
+    while (running) {
         // Gérer les événements (comme la fermeture de la fenêtre)
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_QUIT)
-            {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
                 running = 0;
             }
         }
@@ -191,14 +104,13 @@ int main()
         // Déplacer les drones à chaque cycle
         deplacer_drones(drones, nb_drones);
 
-        // Dessiner l'image de fond (carte)
-        SDL_RenderCopy(renderer, background_texture, NULL, NULL); // Afficher la texture sur toute la fenêtre
+        // Afficher le masque noir (initialement couvrant la carte)
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Noir pour le masque
+        SDL_RenderClear(renderer);  // Remplir toute la fenêtre de noir
 
-
-        // Dessiner les drones
-        for (int i = 0; i < nb_drones; i++)
-        {
-            draw_drone(renderer, &drones[i]);
+        // Révéler la carte progressivement avec les drones
+        for (int i = 0; i < nb_drones; i++) {
+            reveal_map(renderer, map_texture, &drones[i]);
         }
 
         // Mettre à jour l'affichage
@@ -208,7 +120,8 @@ int main()
         SDL_Delay(16);
     }
 
-    SDL_DestroyTexture(background_texture);
+    // Nettoyer les ressources
+    SDL_DestroyTexture(map_texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     IMG_Quit();
